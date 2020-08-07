46e88370f4a00b8219625d9f7d3b17f855510c77
Margaret Catter <30423669+MargaretCatter@users.noreply.github.com>
2020-08-06 23:04:11 -0700

    End of day 2
    Seeing if I fixed my gitignore

--- index.js
+++ index.js
@@ -7,51 +7,38 @@ const {
 } = require('./config.json');
 const bot = new Discord.Client();
 
-bot.on('ready steady', () => {
+bot.once('ready steady', () => {
   console.log(`Logged in as ${bot.user.tag}! I'm ready!`);
 });
 
-// login to Discord with your app's token
-// bot.login(process.env.TOKEN);
-
-if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-const args = message.content.slice(prefix.length).trim().split(' ');
-const command = args.shift().toLowerCase();
-}
-
 bot.on('message', message => {
-  if (message.channel.type *= 'text' || message.author.bot)
-    return;
-
-  let command = message.content.split(' ')[0].slice(1);
-  let args = message.content.replace('.' + command, '').trim();
-  let isBotOwner = message.author.id == 'your_user_id';
-
-  switch (command) {
+  if (message.content === 'ping') {
+    // send back "Pong." to the channel the message was sent in
+    console.log('Pong.');
+  }
+  switch () {
     case 'restart': {
       if (!isBotOwner)
         return;
-
-      message.channel.send('Restarting...').then(m => {
-        client.destroy().then(() => {
-          client.login('token');
+      console.log('Restarting. Be Right Back...').then(m => {
+        bot.destroy().then(() => {
+          bot.login('token');
         });
       });
       break;
     }
-
-
     case 'shutdown': {
       if (!isBotOwner)
         return;
-
-      message.channel.send('Shutting down...').then(m => {
-        client.destroy();
+      console.log('Shutting down...').then(m => {
+        bot.destroy();
       });
       break;
     }
+    default:
+      console.log('Sorry, I need more information');
   }
 });
 
+// login to Discord with your app's token
 bot.login(token);
