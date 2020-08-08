4005afd5ad5589897f0fdda13758fa6b054275df
Margaret Catter <30423669+MargaretCatter@users.noreply.github.com>
2020-08-07 20:18:41 -0700

    3.1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AuroraBot
  
+A Discord Bot Meant for use with [Magical Girls & Mecha](https://margaretcatter.itch.io/magical-girls-mecha) a ttrpg published by Margaret Catter Development. 
+
+Commands coming soon!
+
+## Useful  Commands
+## Battle  Commands
+## Event  Commands
+## Account  Commands
+
+[Made with  💖  by Margaret Catter Development](https://margaretcatterdev.carrd.co/)
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // require('dotenv').config();
 // require the discord.js module
 const Discord = require('discord.js');
+
 const {
   prefix,
-  token
+  token,
+  LittleSister
 } = require('./config.json');
 const bot = new Discord.Client();
 
@@ -11,33 +13,45 @@ bot.once('ready steady', () => {
   console.log(`Logged in as ${bot.user.tag}! I'm ready!`);
 });
 
-bot.on('message', message => {
-  if (message.content === 'ping') {
-    // send back "Pong." to the channel the message was sent in
-    console.log('Pong.');
-  }
-  switch () {
-    case 'restart': {
-      if (!isBotOwner)
+bot.on('message', async (message) => {
+  let isBotOwner = message.author.id.toString() !== LittleSister;
+  let command = message.content.slice(prefix.length);
+
+  if (!isBotOwner) return;
+
+  if (command === 'ownershome')
+    if (!isBotOwner)
+      return;
+  console.log(message.author.id.toString());
+
+  if (command === 'ping')
+    // send back "Pong!" to the console
+    console.log('Pong!');
+
+  switch (command) {
+    case 'restart':
+      if (!isBotOwner) {
+        console.log('Not the owner');
         return;
-      console.log('Restarting. Be Right Back...').then(m => {
-        bot.destroy().then(() => {
-          bot.login('token');
-        });
-      });
+      }
+
+      console.log('Restarting. Be Right Back...');
+      await bot.destroy();
+      await bot.login(token);
       break;
-    }
-    case 'shutdown': {
-      if (!isBotOwner)
+    case 'shutdown':
+      if (!isBotOwner) {
+        console.log('Not the owner');
         return;
-      console.log('Shutting down...').then(m => {
-        bot.destroy();
-      });
+      }
+
+      console.log('Shutting down...');
+      await bot.destroy();
       break;
-    }
     default:
       console.log('Sorry, I need more information');
-  }
+      break;
+  } /* */
 });
 
 // login to Discord with your app's token
